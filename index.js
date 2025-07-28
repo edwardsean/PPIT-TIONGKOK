40ab42fd22165ab3a52f473e3bf1ee11a3014e02
EdwardSeanAlexander <122526355+EdwardSeanAlexander@users.noreply.github.com>
2025-07-29 00:19:58 +0700

    connected to supabase

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import pg from "pg";
 import loginRoute from './route/loginRoute.js'; 
-// import uploadEventRoute from './route/uploadEventRoute.js'; // Assuming you have this route
+import uploadEventRoute from './route/uploadEventRoute.js'; // Assuming you have this route
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+
+const db = new pg.Pool({
+    connectionString: process.env.SUPABASE_DB_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+db.connect();
+export {db}
+
 app.use(express.json());
 app.use("/login", loginRoute);
-// app.use("upload-event-image", uploadEventRoute);
+app.use("/upload-event-image", uploadEventRoute);
 app.use('/static', express.static(join(__dirname, 'static')));
 
 
